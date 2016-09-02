c01ee1c1b5ff2b3bf06d07fc7ac16b4c8426290a
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2016-09-02 09:57:49 +0200

    bim

--- template.js
+++ template.js
@@ -1,5 +1,6 @@
 (function ohMyVoltaire() {    
     var CSS_CLICK_SPAN = ".pointAndClickSpan";
+    var ID_BTN_NEXT = "#btn_question_suivante";
     var VALIDATE_INTERVAL = 10000;
     var cheatsheet = [DUDE_REPLACE_THE_PONEY_MAN];
     setInterval(function() {
@@ -8,7 +9,7 @@
             var bestCorrection = null;
             for(var i = 0; i < cheatsheet.length; i++) {
                 var currentCorrection = cheatsheet[i];
-                if(sentence.indexOf(currentCorrection.Begin.trim()) !== -1 && sentence.indexOf(currentCorrection.End.trim()) !== -1) {
+                if(sentence.indexOf(currentCorrection.Begin) !== -1 && sentence.indexOf(currentCorrection.End) !== -1) {
                     bestCorrection = currentCorrection;
                 }
             }
@@ -28,20 +29,40 @@
 
         var bestMatch = findBestMatch(fullSentence);
         if(bestMatch === null) {
-            console.log("sentence is good, nothing to correct");
+            console.log("cannot find answer, be sure to check before saying that there is not mistake");
         }
         else {  
-            var wrongWord = fullSentence.replace(bestMatch.Begin).replace(bestMatch.End);
-            var splittedSentence = fullSentence.match(/\b(\w+)\b/g);
-            var wrongIndex = splittedSentence.indexOf(wrongWord);
-            console.log(splittedSentence);
-            console.log("wrong word index: " + wrongIndex);
-            $(CSS_CLICK_SPAN).each(function(index) {
-                if(index == wrongIndex) {
-                    console.log("DUDE I FOUND: index=" + index + " word=" + $(this).context.firstChild.data);
-                }
-            });
             console.log(bestMatch);
+
+            var wrongWord = fullSentence.replace(bestMatch.Begin, "").replace(bestMatch.End, "").trim();
+
+            console.log(wrongWord);
+
+            var splittedSentence = fullSentence.match(/(\S+)/g);
+            var splittedWrongWord = wrongWord.split(/([^A-z\u0041-\u00FF]+)/g);
+            
+            console.log(splittedWrongWord);
+            console.log(splittedSentence);
+
+            var sentenceSpans = $(CSS_CLICK_SPAN);
+            var spansLength = sentenceSpans.length;
+
+            for(var i = 0; i < spansLength - splittedWrongWord.length; i++) {
+                for(var j = 0; j < splittedWrongWord.length; j++) {            
+                    var currentSpan = sentenceSpans[i];
+                    var currentWrongPart = splittedWrongWord[j];    
+                    var spanValue = currentSpan.firstChild.data;
+                    console.log("check : span=" + spanValue + " wrong=" + currentWrongPart);
+                    if(spanValue !== currentWrongPart) 
+                        break;                    
+                    console.log("GOOD : span=" + spanValue + " wrong=" + currentWrongPart);
+                    if(j === splittedWrongWord.length - 1) {
+                        currentSpan.click();
+                        $(ID_BTN_NEXT)[0].click();
+                        return;
+                    }                      
+                }
+            }
         }
     }, VALIDATE_INTERVAL);   
 
